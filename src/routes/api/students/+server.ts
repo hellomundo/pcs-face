ab3d98fe1f63aced401f7ba2a330f76655534f7f
Edmundo Ortega <ed.ortega@gmail.com>
2024-02-08 14:10:34 -0800

    added server api

---
+++ src/routes/api/students/+server.ts
@@ -0,0 +1,54 @@
+import type { RequestHandler} from "../$types";
+import { students } from "$lib/db/schema";
+import { tursoClient } from "$lib/server/turso";
+
+import { json } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async (event) => {
+    const options: RequestInit = {
+
+    }
+
+    return new Response("Hello, world!", {
+    });
+}
+
+export const POST: RequestHandler = async ({request}) => {
+    const db = tursoClient();
+
+    const id: string = crypto.randomUUID();
+
+    const {name, face} = await request.json() as {name: string, face: string};
+    
+    try {
+        const newStudent = await db.insert(students).values({ 
+            id, 
+            name, 
+            face 
+        }).returning();
+
+        console.log("Success! New student: ", newStudent);
+
+        return new Response(
+            JSON.stringify(newStudent),
+            {
+                status: 201,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+    } catch {
+        return new Response(
+            JSON.stringify({error: "Error creating student"}),
+            {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+    }
+
+
+}
